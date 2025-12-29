06f268a38d7dc84063367a59bc8184970a022f60
João Vítor <jvnc09229009@gmail.com>
2025-12-29 15:03:54 -0300

    test: configure H2 database and disable swagger for integration tests

--- src/test/java/br/com/VihSousa/invest_plan/InvestmentPlanningApplicationTests.java
+++ src/test/java/br/com/VihSousa/invest_plan/InvestmentPlanningApplicationTests.java
@@ -3,11 +3,21 @@ package br.com.vihsousa.invest_plan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"api.security.token.secret=12345678-segredo-de-teste-muito-longo-para-funcionar",
+
+		// Forces the use of the H2 memory bank.
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+
+		// Turns off the swagger for the test
+		"springdoc.api-docs.enabled=false"
+})
 class InvestmentPlanningApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() { }
 }
